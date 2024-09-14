13d2f253282f8885e64eb03b9a2c01be8ff1203c
Mercury512 <rajoday2002@gmail.com>
2024-09-14 10:21:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,12 +17,12 @@ def is_enabled(value, default):
 
 class Config(object):
     # Bot Information 
-    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "7363400875:AAFoRpKKfvEOgfzzIzYHxwp-h-CCUAxpUh0")
-    TECH_VJ_BOT_USERNAME = os.environ.get("TECH_VJ_BOT_USERNAME", "rk_url_bot") # Bot username without @.
+    TECH_VJ_BOT_TOKEN = os.environ.get("7363400875:AAFoRpKKfvEOgfzzIzYHxwp-h-CCUAxpUh0")
+    TECH_VJ_BOT_USERNAME = os.environ.get("rk_url_bot") # Bot username without @.
     
     # The Telegram API things
-    TECH_VJ_API_ID = int(os.environ.get("TECH_VJ_API_ID", "28993901"))
-    TECH_VJ_API_HASH = os.environ.get("TECH_VJ_API_HASH", "57256c4196b08ffaea61271aca07d7d1")
+    TECH_VJ_API_ID = int(os.environ.get("28993901"))
+    TECH_VJ_API_HASH = os.environ.get("57256c4196b08ffaea61271aca07d7d1")
     
     # the download location, where the HTTP Server runs
     TECH_VJ_DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -47,15 +47,15 @@ class Config(object):
     TECH_VJ_PROCESS_MAX_TIMEOUT = 3600
     
     # your telegram account id
-    TECH_VJ_OWNER_ID = int(os.environ.get("TECH_VJ_OWNER_ID", "1273492006")) 
+    TECH_VJ_OWNER_ID = int(os.environ.get("1273492006")) 
     TECH_VJ_SESSION_NAME = "VJ-URL-UPLOADER-BOT"
     
     # database uri (mongodb)
-    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "mongodb+srv://kalerajkumar77777:qeDfp9e1dw2uKyoS@cluster0.ma4rc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    TECH_VJ_DATABASE_URL = os.environ.get("mongodb+srv://kalerajkumar77777:qeDfp9e1dw2uKyoS@cluster0.ma4rc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     TECH_VJ_MAX_RESULTS = "50"
 
     # channel information
-    TECH_VJ_LOG_CHANNEL = int(os.environ.get("TECH_VJ_LOG_CHANNEL", "-1002476748124")) # your log channel id and make bot admin in log channel with full right 
+    TECH_VJ_LOG_CHANNEL = int(os.environ.get("-1002476748124")) # your log channel id and make bot admin in log channel with full right 
     
    
 
